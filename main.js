a89f1747db65badd12c9861953307935b48f8e9d
iwojdyllo <iwojdyllo@gmail.com>
2020-05-04 20:08:00 +0200

    initial homepage commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+/*alert('hello world! Jak tam Twój nastrój dzisiaj dzisiaj?');*/
+
+/*console.log ('tekst tekst tekst');
+
+alert ('jednak hello world!');
+
+console.log ('alert alert');*/
+
+
+const name='Izabela';
+const name2='Klara';
+const age=39;
+
+
+console.log (name);
+console.log (age);
+
+console.log (`Nazywam się ${name} Irena ${name2} i mam dzisiaj ${age} lat.`)
