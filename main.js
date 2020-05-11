65078c9344f59664c37f25f382f2c4935bfc2d2b
iwojdyllo <iwojdyllo@gmail.com>
2020-05-11 15:27:02 +0200

    BEM model applied

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 /*alert('hello world! Jak tam Twój nastrój dzisiaj dzisiaj?');*/
 
 /*console.log ('tekst tekst tekst');
@@ -7,26 +6,24 @@ alert ('jednak hello world!');
 
 console.log ('alert alert');*/
 
+const name = "Izabela";
+const name2 = "Klara";
+const age = 39;
 
-const name='Izabela';
-const name2='Klara';
-const age=39;
+console.log(name);
+console.log(age);
 
+console.log(`Nazywam się ${name} Irena ${name2} i mam dzisiaj ${age} lat.`);
 
-console.log (name);
-console.log (age);
-
-console.log (`Nazywam się ${name} Irena ${name2} i mam dzisiaj ${age} lat.`);
-
-
-const heading = document.querySelector('.h2--js');
+const heading = document.querySelector(".h2--js");
 
 console.log(heading);
 
 // heading.innerHTML=name;
 
-heading.innerHTML=`Nazywam się ${name} Irena ${name2} i mam dzisiaj ${age} lat.`;
+// heading.innerHTML = `Nazywam się ${name} Irena ${name2} i mam dzisiaj ${age} lat.`;
 
+heading.innerHTML = `Złote myśli na dziś wypisała dla Was ${name} Irena ${name2}`;
 
-const notatkiDod = document.querySelector('.h2__dod--js');
-notatkiDod.innerHTML="Notatki dodatkowe 2";
+const notatkiDod = document.querySelector(".article__header--js");
+notatkiDod.innerHTML = "Notatki dodatkowe 2";
